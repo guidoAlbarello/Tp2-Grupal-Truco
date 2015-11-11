8de5099b072f467231276970fe006197fc655569
jhanvato <jhanvato1101@hotmail.com>
2015-11-11 18:28:04 -0300

    Agregacion clase manejadorDePUntaje

---
+++ src/fiuba/algo3/modelo/ManejadorDePuntaje.java
@@ -0,0 +1,16 @@
+package fiuba.algo3.modelo;
+
+/**
+ * Created by anthony on 11/11/2015.
+ */
+public class ManejadorDePuntaje {
+    public int getPuntaje(String unNombre) {
+        return 0;
+    }
+
+    public void setManosGanadas(String unNombre, int cantidadManosGanadas) {
+    }
+
+    public void sumaPuntaje(String unNombre, int puntajeGanado) {
+    }
+}
