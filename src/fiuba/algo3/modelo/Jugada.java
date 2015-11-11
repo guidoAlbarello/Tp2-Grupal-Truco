2f741b47f6fdc800440aa864437650745e6bbae8
jhanvato <jhanvato1101@hotmail.com>
2015-11-11 13:58:33 -0300

    jjjj

---
+++ src/fiuba/algo3/modelo/Jugada.java
@@ -0,0 +1,11 @@
+package fiuba.algo3.modelo;
+
+/**
+ * Created by anthony on 11/11/2015.
+ */
+public abstract class Jugada {
+    public abstract void resolverEnMesa(Mesa mesa);
+
+    public abstract JugadorEnJuego jugadorQueCanto();
+}
+
