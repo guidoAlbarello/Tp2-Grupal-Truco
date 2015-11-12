948877957812b118ba6733c7464293a27712bb78
guidoAlbarello <guidosergio95@hotmail.com>
2015-11-12 17:46:10 -0300

    "Creacion de logica de la clase ManejadorDePuntaje para manejar los puntos entre dos jugadores con sus respectivos test"

--- src/fiuba/algo3/modelo/ManejadorDePuntaje.java
+++ src/fiuba/algo3/modelo/ManejadorDePuntaje.java
@@ -1,16 +1,25 @@
 package fiuba.algo3.modelo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by anthony on 11/11/2015.
  */
 public class ManejadorDePuntaje {
-    public int getPuntaje(String unNombre) {
-        return 0;
+    private Map<String, Integer> puntajesDelJuego;
+
+    public ManejadorDePuntaje(String nombreJugador1, String nombreJugador2){
+        this.puntajesDelJuego = new HashMap<>();
+        this.puntajesDelJuego.put(nombreJugador1, 0);
+        this.puntajesDelJuego.put(nombreJugador2, 0);
     }
 
-    public void setManosGanadas(String unNombre, int cantidadManosGanadas) {
+    public int getPuntaje(String nombreUnJugador) {
+        return this.puntajesDelJuego.get(nombreUnJugador);
     }
 
     public void sumaPuntaje(String unNombre, int puntajeGanado) {
+        this.puntajesDelJuego.put(unNombre, this.puntajesDelJuego.get(unNombre) + puntajeGanado);
     }
 }
