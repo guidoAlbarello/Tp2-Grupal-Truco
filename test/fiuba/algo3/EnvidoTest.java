5d4e0af6a71db9a865665f6ea53dd32a2c10fd0e
guidoAlbarello <guidosergio95@hotmail.com>
2015-11-19 11:03:25 -0300

    "Refactorizacion test de clase envido"

--- test/fiuba/algo3/EnvidoTest.java
+++ test/fiuba/algo3/EnvidoTest.java
@@ -10,16 +10,10 @@ import org.junit.Test;
 public class EnvidoTest {
     @Test
     public void chequeoDeCalculoCorrectoDelEnvidoEntreDosCartas(){
-        Palo unPalo = new PaloEspada();
-        Palo otroPalo = new PaloBasto();
-        Carta carta1 = new Carta(3, unPalo);
-        Carta carta2 = new Carta(5, unPalo);
-        Carta carta3 = new Carta(7, otroPalo);
-
         Envido envidoACalcular = new EnvidoCartasIgualPalo();
-        Assert.assertEquals(28, envidoACalcular.calcularEnvido(carta1.getValorDeCarta(), carta2.getValorDeCarta()).getValorEnvido());
+        Assert.assertEquals(28, envidoACalcular.calcularEnvido(5,3).getValorEnvido());
 
         envidoACalcular = new EnvidoCartasDiferentePalo();
-        Assert.assertEquals(7, envidoACalcular.calcularEnvido(carta1.getValorDeCarta(), carta3.getValorDeCarta()).getValorEnvido());
+        Assert.assertEquals(7, envidoACalcular.calcularEnvido(7,3).getValorEnvido());
     }
 }
