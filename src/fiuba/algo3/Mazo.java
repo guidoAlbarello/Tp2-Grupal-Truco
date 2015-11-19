8866874a7f6819d60793272561a203e8c9270465
Fechee <fedebogovic@hotmail.com>
2015-11-19 15:39:07 -0300

    Se implemento el mazo con sus tests

---
+++ src/fiuba/algo3/Mazo.java
@@ -0,0 +1,73 @@
+package fiuba.algo3;
+
+import fiuba.algo3.ModeladoDeCarta.*;
+import fiuba.algo3.manejoDeJugadores.Jugador;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+/**
+ * Created by Fechee on 19/11/2015.
+ */
+public class Mazo {
+
+    private LinkedList<Carta> mazoDelJuego;
+    private int numeroDeCartas;
+    private int cartasTotales =40;
+    private LinkedList<Palo> palos;
+    private LinkedList<Integer> numeros;
+    //private LinkedList<Integer> figuras;
+
+
+    public Mazo(){
+        //PALO
+        palos = new LinkedList<Palo>();
+        palos.add ( new PaloEspada() );
+        palos.add ( new PaloBasto() );
+        palos.add ( new PaloCopa() );
+        palos.add ( new PaloOro() );
+        //NUMEROS
+        numeros = new LinkedList<Integer>();
+        numeros.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 10, 11, 12));
+        //figuras = new LinkedList<Integer>();
+        //figuras.addAll(Arrays.asList(10,11,12));
+
+        numeroDeCartas= cartasTotales;
+        mazoDelJuego = new LinkedList<>();
+
+        for (Palo unPalo : palos) {
+            for (Integer  numero : numeros) {
+                Carta unaCarta = new Carta(numero,unPalo);
+                mazoDelJuego.add(unaCarta);
+            }
+        }
+    }
+
+    //ESTE METODO SACA LA CARTA DEL MAZO!!!
+    public Carta repartirUltimaCarta(){
+        return this.mazoDelJuego.removeLast();
+    }
+
+
+    //ENTREGA 3 CARTAS AL JUGADOR
+    /*
+    public void repartirCartasAJugador(Jugador jugador) {
+        jugador.recibirCarta(repartirUltimaCarta());
+        jugador.recibirCarta(repartirUltimaCarta());
+        jugador.recibirCarta(repartirUltimaCarta());
+    }*/
+
+    public int obtenerNumeroDeCartas() {
+        return mazoDelJuego.size();
+    }
+
+
+    public void mezclarMazo(){
+        Collections.shuffle(mazoDelJuego);
+    }
+
+    public LinkedList<Carta> getMazoDelJuego(){
+        return this.mazoDelJuego;
+    }
+}
---
+++ test/fiuba/algo3/MazoTests.java
@@ -0,0 +1,47 @@
+package fiuba.algo3;
+
+import fiuba.algo3.ModeladoDeCarta.*;
+import fiuba.algo3.manejoDeJugadores.Jugador;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Fechee on 19/11/2015.
+ */
+public class MazoTests {
+
+    @Test
+    public void testMazoReparteBienCartasDiferentes(){
+        Mazo unMazo = new Mazo();
+        unMazo.mezclarMazo();
+        Carta carta1 = unMazo.repartirUltimaCarta();
+        Assert.assertEquals(39, unMazo.obtenerNumeroDeCartas());
+        Carta carta2 = unMazo.repartirUltimaCarta();
+        Assert.assertTrue(carta1.getPaloDeCarta()!=carta2.getPaloDeCarta() & carta1.getValorDeCarta()!=carta2.getValorDeCarta());
+        Assert.assertEquals(38, unMazo.obtenerNumeroDeCartas());
+    }
+
+
+    @Test
+    public void testMazoReparteUnaCartaYEstaDesapareceDelMazo(){
+        Mazo unMazo = new Mazo();
+        Carta carta1 = unMazo.repartirUltimaCarta();
+        Palo palo = carta1.getPaloDeCarta();
+        int valor = carta1.getValorDeCarta();
+        for (Carta unaCarta : unMazo.getMazoDelJuego() ){
+            Assert.assertFalse(unaCarta.getPaloDeCarta()==palo  &  unaCarta.getValorDeCarta()==valor);
+        }
+    }
+
+/*
+    @Test
+    public void testMazoReparteTresCartas(){
+        //Cantos cantos = new CantosConFlor();
+        Jugador pedro = new Jugador("pedro");
+        Mazo unMazo = new Mazo();
+        unMazo.mezclarMazo();
+        unMazo.repartirCartasAJugador(pedro);
+        Assert.assertEquals(3,pedro.getMano().getCartasEnMano().size());
+    }
+*/
+}
