c298f44d3adfd82033333cc0642668c63e526c6c
Fechee <fedebogovic@hotmail.com>
2015-11-23 01:01:00 -0300

    Corregi un detalle en este test

--- test/fiuba/algo3/JuegoTest.java
+++ test/fiuba/algo3/JuegoTest.java
@@ -164,13 +164,14 @@ public class JuegoTest {
 
    @Test
     public void seCantaUnEnvidoYLuegoSeJueganCartasJugador1Gana2PuntosDelEnvidoYjuagdor2GanaLaRonda(){
-        jugador1.recibirCarta(new Carta(11,paloOro));
-        jugador1.recibirCarta(new Carta(5,paloBasto));
-        jugador1.recibirCarta(new Carta(6,paloBasto));          // ENVIDO SUPUESTO 31
-
-        jugador2.recibirCarta(new Carta(7,paloBasto));
-        jugador2.recibirCarta(new Carta(6,paloOro));
-        jugador2.recibirCarta(new Carta(3,paloEspada));          //ENVIDO SUPUESTO 33
+        jugador1.recibirCarta(new Carta(11,paloOro));  //carta en posicion 0
+        jugador1.recibirCarta(new Carta(5,paloBasto)); //carta en posicion 1
+        jugador1.recibirCarta(new Carta(6,paloBasto));  //carta en posicion 2
+                                                                                // ENVIDO = 31
+        jugador2.recibirCarta(new Carta(7,paloBasto));   //carta en posicion 0
+        jugador2.recibirCarta(new Carta(6,paloOro));      //carta en posicion 1
+        jugador2.recibirCarta(new Carta(3,paloEspada));   //carta en posicion 2
+                                                                                //ENVIDO = 7
 
         jugador1.envido();
         jugador2.quiero();
