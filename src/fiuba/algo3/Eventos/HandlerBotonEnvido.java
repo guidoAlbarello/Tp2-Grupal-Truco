9267195c82b646105cac7c60fe4d1b2860bb814f
jhanvato <jhanvato1101@hotmail.com>
2015-11-30 23:57:05 -0300

    aa

--- src/fiuba/algo3/Eventos/HandlerBotonEnvido.java
+++ src/fiuba/algo3/Eventos/HandlerBotonEnvido.java
@@ -3,6 +3,12 @@ package fiuba.algo3.Eventos;
 import fiuba.algo3.aplicacionAnthony.Aplicacion;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBoxBuilder;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * Created by anthony on 30/11/2015.
@@ -15,9 +21,15 @@ public class HandlerBotonEnvido implements EventHandler {
     }
     @Override
     public void handle(Event event) {
-        aplicacion.getJuego().manejadorDeTurnos.getJugadorConTurnoActual().envido();
-        aplicacion.getBorderPane().setRight(aplicacion.contenedorDeJugadas());
-        aplicacion.getBorderPane().setCenter(aplicacion.cuadriculaDeJuego());
-        aplicacion.getBorderPane().setLeft(aplicacion.contenedorEstadoDeJuego());
+        try {
+            aplicacion.getJuego().manejadorDeTurnos.getJugadorConTurnoActual().envido();
+            aplicacion.getBorderPane().setRight(aplicacion.contenedorDeJugadas());
+            aplicacion.getBorderPane().setCenter(aplicacion.cuadriculaDeJuego());
+            aplicacion.getBorderPane().setLeft(aplicacion.contenedorEstadoDeJuego());
+        }catch (RuntimeException e){
+            Stage dialogo = new Stage();
+            dialogo.initModality(Modality.WINDOW_MODAL);
+            //dialogo.setScene(new Scene(VBoxBuilder.create().children()));
+        }
     }
 }
